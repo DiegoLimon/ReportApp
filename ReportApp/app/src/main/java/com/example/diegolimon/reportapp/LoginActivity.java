93416de713b462af0c1d80832803141c17fddb65
Diego Limón <diego.limonsep@gmail.com>
2018-02-24 15:35:39 -0600

    new changes, adding image of login activity, changing colors of login activity, hide bar activity of login activity, creating main view with navigation drawer, setting names of the items on navigation drawer, adding floating button to main view.

--- ReportApp/app/src/main/java/com/example/diegolimon/reportapp/LoginActivity.java
+++ ReportApp/app/src/main/java/com/example/diegolimon/reportapp/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.diegolimon.reportapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -92,6 +94,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
     }
 
     private void populateAutoComplete() {
@@ -187,6 +194,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+
         }
     }
 
